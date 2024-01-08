c976ef70c3903c03977a4f9f7e3a5f32d6bc20c9
Do Duc Quan <dodquan@gmail.com>
2024-01-08 08:56:48 +0700

    Disable highlighting

--- src/wireframes/components/animation/AnimationView.tsx
+++ src/wireframes/components/animation/AnimationView.tsx
@@ -1,8 +1,13 @@
 import * as React from "react";
 import './AnimationView.scss';
 import { useEventCallback } from '@app/core';
-import { Diagram, changeItemsAppearance, changeScript, getDiagram, selectItems, useStore } from "@app/wireframes/model";
+import { Diagram, changeScript, getDiagram, useStore } from "@app/wireframes/model";
+// import { Diagram, changeItemsAppearance, changeScript, getDiagram, selectItems, useStore } from "@app/wireframes/model";
 import { useDispatch } from "react-redux";
+// import { texts } from "@app/texts";
+
+// const delimiterRow = texts.common.tableDelimiterRow;
+// const delimiterCol = texts.common.tableDelimiterCol;
 
 interface AnimationViewProps {
 }
@@ -24,60 +29,61 @@ export const AnimationView = ({ diagram }: AnimationViewProps & { diagram: Diagr
             .join('')
     }
 
-    const selectShape = (code: string, selectionStart: number, selectionEnd: number) => {
-        if (selectedDiagram) {
-            const upToSelection = code.substring((selectionStart == selectionEnd) ? 0 : selectionStart, selectionEnd)
-            const selection = upToSelection.split("\n").at(-1) ?? "";
-            const id = (/([^)]+)\[/.exec(selection) ?? [])[1] ?? "";
-            const selectedIndex = (/\[([^)]+)\]/.exec(selection) ?? [])[1].split(",");
-            const selectedRow = parseInt(selectedIndex[0]) - 1;
-            const selectedCol = parseInt(selectedIndex[1]) - 1;
+    // const selectShape = (code: string, selectionStart: number, selectionEnd: number) => {
+    //     if (selectedDiagram) {
+    //         const upToSelection = code.substring((selectionStart == selectionEnd) ? 0 : selectionStart, selectionEnd)
+    //         const selection = upToSelection.split('\n').at(-1) ?? "";
+    //         const id = (/([^)]+)\[/.exec(selection) ?? [])[1] ?? "";
+    //         const selectedIndex = (/\[([^)]+)\]/.exec(selection) ?? [])[1].split(delimiterCol);
+    //         const selectedRow = parseInt(selectedIndex[0]) - 1;
+    //         const selectedCol = parseInt(selectedIndex[1]) - 1;
 
-            // Select shape
-            dispatch(selectItems(selectedDiagram, [id]));
+    //         // Select shape
+    //         dispatch(selectItems(selectedDiagram, [id]));
 
-            // Highlight text
-            const newText = () => {
-                let texts = selectedDiagram.items.get(id)?.text.split('\n') ?? [];
-                if (!Number.isNaN(selectedCol)) {
-                    texts[selectedRow] = (texts[selectedRow].includes('<tspan>')) ? texts[selectedRow] : `${texts[selectedRow].substring(0, selectedCol)}<tspan>${texts[selectedRow].substring(selectedCol, selectedCol + 1)}</tspan>${texts[selectedRow].substring(selectedCol + 1)}`;
-                } else {
-                    texts[selectedRow] = (texts[selectedRow].includes('<tspan>')) ? texts[selectedRow] : `<tspan>${texts[selectedRow]}</tspan>`;
-                }
+    //         // Highlight text
+    //         const newText = () => {
+    //             let texts = selectedDiagram.items.get(id)?.text.split(delimiterRow) ?? [];
+    //             if (!Number.isNaN(selectedCol)) {
+    //                 texts[selectedRow] = (texts[selectedRow].includes('<tspan>')) ? texts[selectedRow] : `${texts[selectedRow].substring(0, selectedCol)}<tspan>${texts[selectedRow].substring(selectedCol, selectedCol + 1)}</tspan>${texts[selectedRow].substring(selectedCol + 1)}`;
+    //             } else {
+    //                 texts[selectedRow] = (texts[selectedRow].includes('<tspan>')) ? texts[selectedRow] : `<tspan>${texts[selectedRow]}</tspan>`;
+    //             }
                 
-                return texts.join('\n');
-            }
-            dispatch(changeItemsAppearance(selectedDiagram, [id], 'TEXT', newText()));
-        }
-    }
+    //             return texts.join(delimiterRow);
+    //         }
+    //         dispatch(changeItemsAppearance(selectedDiagram, [id], 'TEXT', newText()));
+    //     }
+    // }
 
     const changeTextbox = (event: any) => {
         const newCode = event.target.value;
-        const selectionStart = event.target.selectionStart;
-        const selectionEnd = event.target.selectionEnd;
-
+        // const selectionStart = event.target.selectionStart;
+        // const selectionEnd = event.target.selectionEnd;
+        
         addLineNumber(newCode);
         dispatch(doChangeScript(newCode));
-        selectShape(newCode, selectionStart, selectionEnd);
+    //     selectShape(newCode, selectionStart, selectionEnd);
     };
 
-    const revertShape = () => {
-        if (selectedDiagram) {
-            selectedScript.split("\n").forEach((item) => {
-                const id = (/([^)]+)\[/.exec(item) ?? [])[1] ?? "";
-                const originalText = selectedDiagram.items.get(id)?.text.replace('<tspan>','').replace('</tspan>','');
+    // const revertShape = () => {
+    //     if (selectedDiagram) {
+    //         selectedScript.split('\n').forEach((item) => {
+    //             const id = (/([^)]+)\[/.exec(item) ?? [])[1] ?? "";
+    //             const originalText = selectedDiagram.items.get(id)?.text.replace('<tspan>','').replace('</tspan>','');
 
-                dispatch(changeItemsAppearance(selectedDiagram, [id], 'TEXT', originalText));
-            })
-        }
-    }
+    //             dispatch(changeItemsAppearance(selectedDiagram, [id], 'TEXT', originalText));
+    //         })
+    //     }
+    // }
 
     return (
         <div className="code-editor">
             <div className="line-numbers">
                 <span></span>
             </div>
-            <textarea value={selectedScript} onChange={changeTextbox} onMouseLeave={revertShape}/>
+            {/* <textarea value={selectedScript} onChange={changeTextbox} onMouseLeave={revertShape}/> */}
+            <textarea value={selectedScript} onChange={changeTextbox} />
         </div>
     );
 };
